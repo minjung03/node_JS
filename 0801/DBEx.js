00c412b8b3c0ad5c84f16004599ebe545c60f204
minjung Kim <ikwmjj@naver.com>
2022-08-01 15:12:00 +0900

    0801 Node.js 실습 커밋 (DB 연동)

--- 0801/DBEx.js
+++ 0801/DBEx.js
@@ -1,28 +1,77 @@
 // 미들웨어 추가
 var fs = require('fs');
 var ejs = require('ejs');
-var http = reauire('http');
+var http = require('http');
 var express = require('express');
+var static = require('serve-static');
+var path = require("path");
 var bodyParser = require('body-parser');
-
-// express 서버 실행
-var app = express();
-app.set('prot', process.env.PORT || 4444);
-
 // 모듈 추출
 var mysql = require('mysql'); 
 
+// express 서버 실행
+var app = express();
+app.set('port', process.env.PORT || 4444);
+
+app.use('/public', static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended : false
+}));
+app.use(bodyParser.json());
+
 // 데이터베이스와 연결
-var db = mysql.createConnection({
-    host: 'localhost',
+var client = mysql.createConnection({
     user: 'root',
-    port : 3306,
     password: '111111',
     database: 'company'
 });
 
+// 서버 실행
+app.listen(4444, function(){
+    console.log('server running at http//127.0.0.1:4444...')
+});
+
+// 데이터 표시
+app.get('/', function(req, res){
+    
+    fs.readFile('DBEx_list.html', 'utf8', function(error, data){
+        client.query('select * from products', function(err, result){
+            res.send(ejs.render(data, {
+                data : result
+            })); 
+        });
+    });
+    // res.send('hi');
+});
+
+// 데이터 삭제
+app.get('/delete/:id', function(req, res){
+    client.query('delete from products where id=?', [req.params.id], function(){
+        console.log('--------'+[req.params.id]+'번이 삭제되었습니다');
+        res.redirect('/');
+    });
+});
+
+// 데이터 추가
+app.get('/insert', function(req, res){
+    fs.readFile('DBEx_add.html', 'utf-8',function(error, data){
+        res.send(data);
+    })
+});
+app.post('/insert', function(req, res){
+    var body = req.body;
+
+    client.query('insert into products(name, modelnumber, series) values (?,?,?)',
+    [body.name, body.modelnum, body.series], function(){
+        console.log('--------데이터가 추가되었습니다.');
+        res.redirect('/');
+    });
+});
+
 // 데이터베이스 쿼리 실행
-db.query('select * from products', function(error, result, fields){
+client.query('select * from products', function(error, result, fields){
     if(error) console.log('에러 발생 : '+error);
-    else console.log(result);
+    else {
+        // console.log(result);
+    }
 });
